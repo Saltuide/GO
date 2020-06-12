d5ae268f3c63b9d6ba0664c33f1cb3ad9bf9de2c
Saltuide <goonerus38@gmail.com>
2020-06-12 14:28:05 +0800

    проверяй

--- main.py
+++ main.py
@@ -2,95 +2,133 @@ import os
 import sys
 
 import pygame
-from tkinter import *
 
+from tkinter import *
+from tkinter import scrolledtext
+
+
+WHITE = (255, 255, 255)
+GRAY = (200, 200, 200)
+BLACK = (0, 0, 0)
 
 def done():
     global run
     run = False
 
+
 class Game:
     def __init__(self):
-        self.white = (255, 255, 255)
-        self.gray = (200, 200, 200)
-        self.black = (0, 0, 0)
         self.width = 1200
-        self.height = 900
+        self.height = 800
+        self.black_points = 0
+        self.white_points = 0
         ######################################
         self.root = Tk()
-        menubar = Menu(self.root)
-        filemenu = Menu(menubar, tearoff=0)
-        filemenu.add_command(label="New", command=self.donothing)
-        filemenu.add_command(label="Open", command=self.donothing)
-        filemenu.add_command(label="Save", command=self.donothing)
-        filemenu.add_command(label="Save as...", command=self.donothing)
-        filemenu.add_command(label="Close", command=self.donothing)
+        self.root.title("Go")
+        self.root.iconbitmap('icon.ico')
+        self.root.configure(bg = 'red')
 
-        filemenu.add_separator()
+        embed = Frame(self.root, width=self.width - 330, height=self.height)
+        embed.pack(side = LEFT)
 
-        filemenu.add_command(label="Exit", command=self.root.quit)
-        menubar.add_cascade(label="File", menu=filemenu)
-        editmenu = Menu(menubar, tearoff=0)
-        editmenu.add_command(label="Undo", command=self.donothing)
+        history_win_color = '#%02x%02x%02x' % GRAY #i dunno why it works so weird
 
-        editmenu.add_separator()
+        self.history_text = scrolledtext.ScrolledText(self.root , width = 30, height = 30)
+        self.history_text.config(state = DISABLED, font = ("Times New Roman", "14", "bold")) # readonly
+        self.history_text.pack(pady = 20)
 
-        editmenu.add_command(label="Cut", command=self.donothing)
-        editmenu.add_command(label="Copy", command=self.donothing)
-        editmenu.add_command(label="Paste", command=self.donothing)
-        editmenu.add_command(label="Delete", command=self.donothing)
-        editmenu.add_command(label="Select All", command=self.donothing)
+        pass_button = Button(self.root, text = "Pass", height = 2, width = 10)
+        pass_button.config(command = self.skip_turn, font= ("Times New Roman", "16", "bold"))
+        pass_button.pack()
 
-        menubar.add_cascade(label="Edit", menu=editmenu)
-        helpmenu = Menu(menubar, tearoff=0)
-        helpmenu.add_command(label="Help Index", command=self.donothing)
-        helpmenu.add_command(label="About...", command=self.donothing)
-        menubar.add_cascade(label="Help", menu=helpmenu)
-
-        self.root.config(menu=menubar)
-
-        embed = Frame(self.root, width=self.width, height=self.height)
-        embed.pack()
         self.root.resizable(width=False, height=False)
         
-        
-
         # Tell pygame's SDL window which window ID to use
         os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+        
         # Show the window so it's assigned an ID.
         self.root.update()
         self.root.protocol("WM_DELETE_WINDOW", done)
 
         pygame.init()
-        
-        self.win = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("ГО")
-        icon = pygame.image.load('icon.png')
-        pygame.display.set_icon(icon)
+        self.win = pygame.display.set_mode((self.width - 330, self.height))
+
         self.coords = {} #паре координат будет соотвествовать пара индексов матрицы
         self.block_size = 40
         self.whos_turn = True #ход черного
-        self.draw_base_field()
+        self.menubar()
 
+        self.myfont = pygame.font.SysFont("Times New Roman", 15, "bold")
+        label = self.myfont.render("Черные: 0    Белые: 0", 1, (0,0,0))
+        self.win.blit(label, (680, 30))
+
+        self.draw_base_field(14, 14)
+
+    def skip_turn(self):
+        print("skipped")
+
+    def menubar(self):
+        menubar = Menu(self.root)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label = "New", command = self.donothing)
+        filemenu.add_command(label = "Open", command = self.donothing)
+        filemenu.add_command(label = "Save", command = self.donothing)
+        filemenu.add_command(label = "Replay", command = self.donothing)
+        #filemenu.add_command(label="Save as...", command=self.donothing)
+        #filemenu.add_command(label="Close", command=self.donothing)
+        filemenu.add_separator()
+        filemenu.add_command(label = "Exit", command = done)
+        menubar.add_cascade(label = "Game", menu = filemenu)
+
+        editmenu = Menu(menubar, tearoff = 0)
+        # editmenu.add_command(label = "Undo", command = self.donothing)
+        # editmenu.add_separator()
+        # editmenu.add_command(label = "Cut", command = self.donothing)
+        # editmenu.add_command(label = "Copy", command = self.donothing)
+        # editmenu.add_command(label = "Paste", command = self.donothing)
+        # editmenu.add_command(label = "Delete", command = self.donothing)
+        # editmenu.add_command(label = "Select All", command = self.donothing)
+        sizemenu = Menu(editmenu, tearoff = 0)
+        sizemenu.add_command(label = "9x9", command = lambda: self.draw_base_field(8, 8))
+        sizemenu.add_command(label = "11x11", command = lambda: self.draw_base_field(10, 10))
+        sizemenu.add_command(label = "13x13", command = lambda: self.draw_base_field(12, 12))
+        sizemenu.add_command(label = "15x15", command = lambda: self.draw_base_field(14, 14))
+        sizemenu.add_command(label = "17x19", command = lambda: self.draw_base_field(16, 18))
+        sizemenu.add_command(label = "11x19", command = lambda: self.draw_base_field(10, 18))
+
+        editmenu.add_cascade(label = "Change size", menu = sizemenu)
+
+        menubar.add_cascade(label = "Edit", menu = editmenu)
+        # helpmenu = Menu(menubar, tearoff=0)
+        # helpmenu.add_command(label="Help Index", command=self.donothing)
+        # helpmenu.add_command(label="About...", command=self.donothing)
+        # menubar.add_cascade(label="Help", menu=helpmenu)
+
+        self.root.config(menu = menubar)
 
     def donothing(self):
         print("kappa")
 
-
-    def draw_base_field(self):
-        #self.block_size = 40 #Set the size of the grid block
-        field_width = field_height = 14
+    def draw_base_field(self, x, y):
+        self.coords = {}
+        field_width = x 
+        field_height = y
         up_space = 20
         left_space = 20
 
-        main_surf = pygame.Surface((self.width, self.height))
-        main_surf.fill(self.gray)
+        main_surf = pygame.Surface((self.width - 330, self.height))
+        main_surf.fill(GRAY)
         self.win.blit(main_surf, (0, 0))
+       
 
         self.game_surf = pygame.Surface((self.block_size * field_width,
                                     self.block_size * field_height))
-        self.game_surf.fill(self.white)
+        self.game_surf.fill(WHITE)
+
         self.win.blit(self.game_surf, (up_space, left_space))
+
+        label = self.myfont.render("Черные: 0    Белые: 0", 1, (0,0,0))
+        self.win.blit(label, (680, 30))
 
         for x in range(field_width + 1):
             for y in range(field_height + 1):
@@ -101,9 +139,17 @@ class Game:
                     continue
                 rect = pygame.Rect(block_coord_x, block_coord_y,
                                     self.block_size, self.block_size)
-                pygame.draw.rect(self.win, self.black, rect, 1)
+                pygame.draw.rect(self.win, BLACK, rect, 1)
 
         pygame.display.update()
+
+    def add_turn_info(self, x, y):
+        self.history_text.config(state = NORMAL)
+        if self.whos_turn:
+            self.history_text.insert(INSERT, f"Черные сделали ход в {x}:{y}\n")
+        else:
+            self.history_text.insert(INSERT, f"Белые сделали ход в {x}:{y}\n")
+        self.history_text.config(state = DISABLED)
 
     def place_stones(self, x, y):
         field_size_x, field_size_y = self.game_surf.get_size()
@@ -113,6 +159,8 @@ class Game:
         else:
             tmp_x = round((x - 20) / self.block_size) #смотрим к какому пересечению мы ближе
             tmp_y = round((y - 20) / self.block_size)
+
+            self.add_turn_info(tmp_x, tmp_y)
 
             tmp_x = 20 + self.block_size * tmp_x #вычисляем координаты этого пересечения
             tmp_y = 20 + self.block_size * tmp_y
@@ -126,40 +174,26 @@ class Game:
             self.whos_turn = not self.whos_turn
             pygame.display.update()
 
-    def run(self):
-        #run = True
-        
+    
 
+
+    def run(self):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.place_stones(event.pos[0], event.pos[1])
-        #while run:
-            # def done():
-            #     global run
-            #     run = False
-            
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         run = False
-            #     elif event.type == pygame.MOUSEBUTTONDOWN:
-            #         self.place_stones(event.pos[0], event.pos[1])
-        pygame.display.update()  # (or pygame.display.update())
+  
+        pygame.display.update()  
         self.root.update_idletasks()
         self.root.update()
-            #pygame.time.wait(1000)
-
-            
-        # pygame.quit()
-        # sys.exit()
 
 
-game = Game()
+if __name__ == "__main__":         
+    game = Game()
+    run = True
+    while run:
+        game.run()
 
-run = True
-while run:
-    game.run()
-
-pygame.quit()
+    pygame.quit()
 
 
 
--- test.py
+++ test.py
@@ -1,74 +1,30 @@
-from tkinter import *
-
 import pygame
-
+import tkinter as tk
+from tkinter import *
 import os
-import sys
 
-def donothing():
-   print("kappas")
-
-root = Tk()
-
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=donothing)
-filemenu.add_command(label="Open", command=donothing)
-filemenu.add_command(label="Save", command=donothing)
-filemenu.add_command(label="Save as...", command=donothing)
-filemenu.add_command(label="Close", command=donothing)
-
-filemenu.add_separator()
-
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label="Undo", command=donothing)
-
-editmenu.add_separator()
-
-editmenu.add_command(label="Cut", command=donothing)
-editmenu.add_command(label="Copy", command=donothing)
-editmenu.add_command(label="Paste", command=donothing)
-editmenu.add_command(label="Delete", command=donothing)
-editmenu.add_command(label="Select All", command=donothing)
-
-menubar.add_cascade(label="Edit", menu=editmenu)
-helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="Help Index", command=donothing)
-helpmenu.add_command(label="About...", command=donothing)
-menubar.add_cascade(label="Help", menu=helpmenu)
-
-root.config(menu=menubar)
-
-embed = Frame(root, width=640, height=480)
-embed.pack()
-root.resizable(width=False, height=False)
-root.mainloop()
-
-# Tell pygame's SDL window which window ID to use
+root = tk.Tk()
+embed = tk.Frame(root, width = 500, height = 500) #creates embed frame for pygame window
+# embed.grid(columnspan = (600), rowspan = 500) # Adds grid
+embed.pack(side = LEFT) #packs window to the left
+buttonwin = tk.Frame(root, width = 75, height = 500)
+buttonwin.pack(side = LEFT)
 os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
-# Show the window so it's assigned an ID.
-root.update()
-
-# Usual pygame initialization
-pygame.init()
-
-# Dimensions should match those used in the embed frame
-screen = pygame.display.set_mode((640, 480))
-
-running = True
-def done():
-    # global running
-    # running = False
-    # pygame.quit()
-    root.destroy()
-    exit()
-
-root.protocol("WM_DELETE_WINDOW", done)
-while running:
-    # game logic goes here
-    pygame.display.flip()  # (or pygame.display.update())
-    root.update_idletasks()
+os.environ['SDL_VIDEODRIVER'] = 'windib'
+screen = pygame.display.set_mode((500,500))
+screen.fill(pygame.Color(255,255,255))
+pygame.display.init()
+pygame.display.update()
+def draw(x):
+    pygame.draw.circle(screen, (0,0,0), (x[0],x[1]), 15)
+    pygame.display.update()
+   
     root.update()
-pygame.quit()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            draw(event.pos)
+
+    pygame.display.update()
+    root.update()
